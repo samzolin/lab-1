a44f252b47cccd41f863fd886c14c5e9ae8f9b19
samzolin <saz5193@psu.edu>
2020-09-03 18:24:36 +0000

    added parenthesis

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ inp = input("Enter temperature: ")
 unit = input("Enter unit in F/f or C/c: ")
 temp = float(inp)
 temp1 = temp * 9/5 + 32
-temp2 = temp - 32 * 9/5
+temp2 = (temp - 32) * 9/5
 if unit == "F" or unit == "f":
   print(f"{temp}° in Fahrenheit is equivalent to {temp2}°Celsius.")
 elif unit == "C" or unit == "c":
-  print(f"{temp}° in Celsius is equivalent to {temp1}°Fahrenheit.")
+  print(f"{temp}° in Celsius is equivalent to {temp1}° Fahrenheit.")
 else:
   print(f"Invalid unit({unit}).")
